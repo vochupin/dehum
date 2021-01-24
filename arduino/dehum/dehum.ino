b55585e2e0ea2dd361e9f34fdb3eebbef4e59c6f
Vladimir Chupin <vpchupin@gmail.com>
2021-01-25 00:39:50 +0700

    save parameters to eeprom

--- arduino/dehum/dehum.ino
+++ arduino/dehum/dehum.ino
@@ -5,11 +5,19 @@
 #include <Thread.h>             // https://github.com/ivanseidel/ArduinoThread
 #include <ThreadController.h>
 #include <ArduinoJson.h>
+#include <EEPROM.h>
 #include "SevenSegmentTM1637.h"
 
 
 #include "DHTesp.h"
 #include "settings.h"
+
+#define EEPROM_SIZE 9 
+
+//EEPROM Map
+#define EE_SETPOINT 0
+#define EE_HYSTERESIS 4
+#define EE_MODE 8
 
 WiFiClient espClient;
 PubSubClient client(MQTT_SERVER, 1883, espClient);
@@ -47,8 +55,21 @@ StaticJsonDocument<256> doc;
 
 const String CMD_ENABLE = String("enable");
 const String CMD_DISABLE = String("disable");
+const String CMD_MODE_REMOTE = String("remote");
+const String CMD_MODE_AUTO = String("auto");
 
 const char* KEY_FAN = "fan";
+const char* KEY_FAN_SETPOINT = "setpoint";
+const char* KEY_FAN_HYSTERESIS = "hysteresis";
+const char* KEY_FAN_MODE = "mode";
+
+float setpoint;
+float hysteresis;
+
+#define MODE_AUTO 0
+#define MODE_REMOTE 1
+
+byte mode;
 
 /**
  * initTemp
@@ -145,12 +166,80 @@ String getTemperature() {
   return output;
 }
 
+void writeFloatToEeprom(word eepromAdr, float data) {
+  byte* b = (byte*) &data;
+  EEPROM.write(eepromAdr, b[0]);
+  EEPROM.write(eepromAdr + 1, b[1]);
+  EEPROM.write(eepromAdr + 2, b[2]);
+  EEPROM.write(eepromAdr + 3, b[3]);
+  
+}
+
+void initVariables() {
+  setpoint = 50.0;
+  hysteresis = 5.0;
+  mode = MODE_AUTO;
+
+  writeFloatToEeprom(EE_SETPOINT, setpoint);
+
+  writeFloatToEeprom(EE_HYSTERESIS, hysteresis);
+
+  EEPROM.write(EE_MODE, mode);
+
+  EEPROM.commit();  
+
+  Serial.println("Initialize setpoint: " + String(setpoint));
+  Serial.println("Initialize hysteresis: " + String(hysteresis));
+  Serial.println("Initialize mode: " + String(mode));
+}
+
+void readVariablesFromEeprom() {
+  byte* b = (byte*) &setpoint;
+
+  byte needInit = 0xff;
+  
+  b[0] = EEPROM.read(EE_SETPOINT);
+  needInit &= b[0];
+  b[1] = EEPROM.read(EE_SETPOINT + 1);
+  needInit &= b[1];
+  b[2] = EEPROM.read(EE_SETPOINT + 2);
+  needInit &= b[2];
+  b[3] = EEPROM.read(EE_SETPOINT + 3);
+  needInit &= b[3];
+
+  if (needInit == 0xff) {
+    initVariables();
+    return;
+  }
+
+  b = (byte*) &hysteresis;
+  
+  b[0] = EEPROM.read(EE_HYSTERESIS);
+  b[1] = EEPROM.read(EE_HYSTERESIS + 1);
+  b[2] = EEPROM.read(EE_HYSTERESIS + 2);
+  b[3] = EEPROM.read(EE_HYSTERESIS + 3);
+
+  mode = EEPROM.read(EE_MODE);
+
+  Serial.println("Setpoint: " + String(setpoint));
+  Serial.println("Hysteresis: " + String(hysteresis));
+  Serial.println("Mode: " + String(mode));
+}
+
 void setup() {
   pinMode(PIN_RELAY, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+
+  EEPROM.begin(EEPROM_SIZE);
   
   Serial.begin(115200);
   Serial.println();
+
+  readVariablesFromEeprom();
+
+  display.begin();            // initializes the display
+  display.setBacklight(100);  // set the brightness to 100 %
+  display.print("INIT");      // display INIT on the display
 
   // Connect to WiFi
   Serial.print(s+"Connecting to WiFi: "+WIFI_SSID+" ");
@@ -170,11 +259,8 @@ void setup() {
   } else {
     Serial.println(s+"failed, rc="+client.state());
   }
-
-  display.begin();            // initializes the display
-  display.setBacklight(100);  // set the brightness to 100 %
-  display.print("INIT");      // display INIT on the display
-  delay(1000);                // wait 1000 ms
+  
+  display.print("conn");      // display INIT on the display
   
   // Enable Threads
   thread.onRun(publisher);
@@ -245,5 +331,25 @@ void topic_subscriber(String topic, String message) {
     } else if (CMD_DISABLE.equalsIgnoreCase(fanStr)) {
       relayState = false;
     }
+  } else if (doc.containsKey(KEY_FAN_SETPOINT)) {
+    String setpointStr = doc[KEY_FAN_SETPOINT];
+  
+    setpoint = setpointStr.toFloat();
+
+    writeFloatToEeprom(EE_SETPOINT, setpoint);
+  
+    EEPROM.commit();  
+
+    Serial.println("Write setpoint to EEPROM: " + String(setpoint));
+  } else if (doc.containsKey(KEY_FAN_HYSTERESIS)) {
+    String hysteresisStr = doc[KEY_FAN_HYSTERESIS];
+  
+    hysteresis = hysteresisStr.toFloat();
+
+    writeFloatToEeprom(EE_HYSTERESIS, hysteresis);
+  
+    EEPROM.commit();  
+
+    Serial.println("Write hysteresis to EEPROM: " + String(hysteresis));
   }
 }
